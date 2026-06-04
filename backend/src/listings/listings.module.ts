c92c5f0987e70b976097d285ce7eaa292885dcde
chdeveloppement42 <ch.developpement42@gmail.com>
2026-06-04 11:02:48 +0100

    last change

--- backend/src/listings/listings.module.ts
+++ backend/src/listings/listings.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ListingsController } from './listings.controller';
 import { ListingsService } from './listings.service';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [NotificationsModule, StorageModule],
   controllers: [ListingsController],
   providers: [ListingsService],
+  exports: [ListingsService],
 })
 export class ListingsModule {}
